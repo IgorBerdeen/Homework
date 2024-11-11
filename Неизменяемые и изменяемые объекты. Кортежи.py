1638dc687252930ba7422bd82079939634fb8ac6
Игорь <119707927+IgorBerdeen@users.noreply.github.com>
2024-11-11 22:11:04 +0300

    Add files via upload

---
+++ Неизменяемые и изменяемые объекты. Кортежи.py
@@ -0,0 +1,8 @@
+immutable_var = 'One', True, 3
+print(immutable_var)
+#immutable_var[1] = 2
+#Элементы кортежа нельзя изменить, за исключением случев,
+#когда в кортеже создается список
+mutable_list = ['Один', 2, True]
+mutable_list[0] = 'One'
+print(mutable_list)
