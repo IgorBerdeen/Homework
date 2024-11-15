4cd95f60eec8143ae35867627d0642f08c3eef98
Игорь <119707927+IgorBerdeen@users.noreply.github.com>
2024-11-15 08:38:37 +0300

    Add files via upload

--- Базовые структуры данных.py
+++ Базовые структуры данных.py
@@ -1,14 +1,16 @@
-#1st program
-print((9**0.5)*5)
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+grades = [sum(grades[0])/len(grades[0]),
+           sum(grades[1])/len(grades[1]),
+           sum(grades[2])/len(grades[2]),
+           sum(grades[3])/len(grades[3]),
+           sum(grades[4])/len(grades[4]),]
+students = sorted(students)
+class_dict = {students[0]:grades[0],
+              students[1]:grades[1],
+              students[2]:grades[2],
+              students[3]:grades[3],
+              students[4]:grades[4],}
+print(class_dict)
 
-#2nd program
-print(99.9>99.8 and 1000!=1000.1)
 
-#3rd program
-print(2*2+2)
-print(2*(2+2))
-print((2*2+2)==(2*(2+2)))
-
-#4th program
-# дана строка '123.456'
-print(int(float('123.456')*10%10))
